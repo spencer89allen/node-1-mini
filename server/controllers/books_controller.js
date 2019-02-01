a1b1c8b7a4eff5b717c7f424f0cf5ce32ebc1bfb
Spencer Allen <spencerallen@Spencers-MacBook-Pro.local>
2019-02-01 14:50:20 -0700

    steps 1 through 9 complete

--- server/controllers/books_controller.js
+++ server/controllers/books_controller.js
@@ -24,6 +24,31 @@ module.exports = {
         res.status('200').send(books)
     },
 
+    update: function(req, res) {
 
+        let index = null;
+        books.forEach((book, i) => {
+          if(book.id === Number(req.params.id)) index = i;
+        })
+        books[ index ] = {
+          id: books[ index ].id,
+          title: req.body.title || books[ index ].title,
+          author: req.body.author || books[ index ].author
+        }
+        // I need practice with js but really understanding what is being done above and stuff
+
+        res.status('200').send(books)
+    },
+
+    delete: function() {
+
+        var index = null
+        books.forEach((book, i) => {
+            if(book.id === Number(req.params.id)) index = i
+        })
+        books.splice(index, 1)
+
+        res.status(200).send( books );
+    }
 
 }
--- server/index.js
+++ server/index.js
@@ -11,7 +11,10 @@ app.use(bodyParser.json())
 
 // Endpoints
 app.get('/api/books', bc.read())
-app.post('/api/books', bc.create)
+app.post('/api/books', bc.create())
+app.put(`/api/books:id`, bc.update())
+// why is it a collen and not ${} or something?
+app.delete(`/api/books:id`, bc.delete())
 
 var port = 4000
 app.listen(port, () => {
